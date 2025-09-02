487ec57a43aeb27846171feb9620045f63a7fec9
ThivankaD <121705081+ThivankaD@users.noreply.github.com>
2025-09-02 09:50:21 +0530

    correct

--- app/src/main/java/org/example/Task.java
+++ app/src/main/java/org/example/Task.java
@@ -1,37 +1,13 @@
 package org.example;
 
-import java.util.Scanner;
-
 public class Task {
-
-    private String name;
-    private String nameCopy;
+     private String name;
 
     public Task(String name) {
         this.name = name;
-        this.nameCopy = name;
     }
 
     public String getName() {
         return name;
-    }
-
-    public String getNameCopy() {
-        return nameCopy;
-    }
-
-    public void printTask() {
-        System.out.println("Task name: " + name);
-    }
-
-    public void printTaskAgain() {
-        System.out.println("Task name: " + name);
-    }
-
-    public void getUserInput() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter task name:");
-        String input = scanner.nextLine();
-        this.name = input;
     }
 }
